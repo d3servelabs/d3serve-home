522297e74388c12e0bd85b934b0507c77398b5b2
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2024-12-30 11:14:01 -0400

    -

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,22 +5,22 @@ export const URL = env.NEXT_PUBLIC_VERCEL_URL
   : (env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000");
 
 export const SOCIALS = {
-  discord: "https://discord.com",
-  twitter: "https://twitter.com",
-  github: "https://github.com",
-  telegram: "https://t.me",
-  linkedin: "https://linkedin.com",
+  discord: "https://discord.namefi.gg",
+  twitter: "https://twitter.com/d3serve",
+  github: "https://github.com/d3servelabs",
+  telegram: "https://t.me/t.me/namefidao",
+  linkedin: "https://www.linkedin.com/company/98253007",
 };
 
 export const CONTACTS = {
-  email: "a@b.com",
-  phone: "+1234567890",
+  email: "zzn@d3serve.xyz",
+  phone: "",
 };
 
 export const WEBSITE = {
-  name: "Home",
-  title: "Home",
-  description: "Home",
+  name: "D3Serve Labs",
+  title: "D3Serve Labs",
+  description: "D3Serve Labs",
   url: URL,
   icon: `${URL}/icon.png`,
   color: "#000000",
