a510550412bb842f45b6dfea68d0392245d5e59f
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2025-01-07 12:27:24 -0400

    -

---
+++ src/app/subscribers/page.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@/components/Container";
+import { mongoConnect, Subscriber } from "@/mongo";
+
+export default async function Page() {
+  const connection = await mongoConnect();
+
+  const count = await Subscriber.countDocuments();
+
+  await connection.disconnect();
+
+  return (
+    <div className="flex w-full flex-1 flex-col gap-8 p-8">
+      <Container className="space-x-8 space-y-8">Subscribers {count}</Container>
+    </div>
+  );
+}
