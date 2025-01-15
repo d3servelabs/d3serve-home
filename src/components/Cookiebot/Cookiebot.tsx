6d2e04db8dceee7d7615d0837aa9a2de9cde044c
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2025-01-15 15:45:18 -0400

    Avoid mongodb rejections

--- src/components/Cookiebot/Cookiebot.tsx
+++ src/components/Cookiebot/Cookiebot.tsx
@@ -1,13 +1,18 @@
 import Script, { ScriptProps } from "next/script";
 import { FC } from "react";
+
 export type CookiebotProps = ScriptProps & {
   settingsId?: string;
+  active?: boolean;
 };
+
 export const Cookiebot: FC<CookiebotProps> = ({
   settingsId,
+  active = true,
   ...rest
 }: CookiebotProps) => {
-  if (!settingsId) return null;
+  if (!settingsId || !active) return null;
+
   return (
     <Script
       id="usercentrics-cmp"
