b05329c89f34ffd54b55560a0aa606447c863dbd
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2025-01-15 15:39:42 -0400

    Avoid mongodb rejections

--- src/mongo/models/Subscriber.ts
+++ src/mongo/models/Subscriber.ts
@@ -1,25 +1,27 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
-const SubscriberSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
+const SubscriberSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      default: uuidv4, // Automatically generates a UUID for each document
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true },
+);
 
 export default mongoose.models.Subscriber ||
   mongoose.model("Subscriber", SubscriberSchema);
